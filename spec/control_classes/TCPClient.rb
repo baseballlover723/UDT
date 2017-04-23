777508db236dcf5e59b29cbcbbafc4c5d33e93b0
Philip Ross <rosspa@rose-hulman.edu>
2017-04-23 18:15:46 -0400

    added client and server mixin. renamed file to use snake case

--- spec/control_classes/TCPClient.rb
+++
@@ -1,3 +0,0 @@
-class TCPClient
-
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "udt"
 $LOAD_PATH.unshift File.expand_path("../control_classes", __FILE__)
-require "TCPServer"
-require "TCPClient"
-require "UDPServer"
-require "UDPClient"
+require "client"
+require "server"
+require "tcp_server"
+require "tcp_client"
+require "udp_server"
+require "udp_client"
