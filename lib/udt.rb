0f9a5c5936c785f1bf53b6f3b7ddfc5c9fa7b7ba
Philip Ross <rosspa@rose-hulman.edu>
2017-05-11 16:16:49 -0400

    implemented receive, hopefully

--- lib/udt.rb
+++ lib/udt.rb
@@ -44,7 +44,28 @@ module Udt
   end
 
   def receive
+    command = wait_command until command[:name] == 'start' # ????
+    packets = command[:data]
+    data = {}
+    acks = Set.new
+    thread = Thread.new do
+      while true
+        sleep 1
+        send_acks! acks
+      end
+    end
+    while true
+      data_obj = wait_data
+      data_chunk = data_obj[:data]
+      index = data_obj[:index]
+      data[index] = data_chunk
+      acks << index
+      break if data.size == packets
+    end
+    send_acks! acks
+    thread.exit
 
+    piece_together data
   end
 
   private
@@ -59,9 +80,21 @@ module Udt
     end
   end
 
+  def piece_together(data)
+    data_string = ''
+    for index in 0...data.size
+      data_string << data[index]
+    end
+    data_string
+  end
+
   def wait_command
     # {name: '', data: {}}
     JSON.parse(@tcp.recv(PACKET_SIZE))
+  end
+
+  def wait_data
+    JSON.parse @udp.recv(PACKET_SIZE)
   end
 
   def send_command(command, data)
@@ -72,5 +105,10 @@ module Udt
   def send_data(index)
     json = {data: @data[index], index: index}
     @udp.send(JSON.stringify(json), 0, @host, @port)
+  end
+
+  def send_acks!(acks)
+    send_command(:ack, acks)
+    acks.clear
   end
 end
