3dd4de58b8b7a3383bce29eaaaed47e2acb8256f
Philip Ross <rosspa@rose-hulman.edu>
2017-05-11 11:49:51 -0400

    8/upgrade benchmark (#10)
    * benchmark now measures the time it takes each file to transmit
    
    * removed failed print, increased sleep on file delete error

--- spec/control_classes/client.rb
+++ spec/control_classes/client.rb
@@ -8,7 +8,7 @@ module Client
   end
 
   def read_file(file_path, size)
-    File.open(file_path) do |file|
+    File.open(file_path, "rb") do |file|
       counter = 0
       while (buffer = file.read(size)) do
         yield buffer, counter
--- spec/control_classes/tcp_control_client.rb
+++ spec/control_classes/tcp_control_client.rb
@@ -3,49 +3,34 @@ require 'socket'
 class TCPControlClient
   include Client
   include Server
-  @socket
+  PACKET_SIZE = 1024
 
   def initialize(host, port)
     @socket = TCPSocket.new(host, port)
   end
 
   def send(file_path)
-    begin
-      unless File.exists?(file_path)
-        @socket.close
-        raise Exception, 'File does not exist'
-      end
-
-      total_file_contents = ''
-      begin
-        file = File.open(file_path, 'r')
-        total_file_contents = file.read
-      rescue Exception => e
-        @socket.close
-        raise e
-      end
-
-      @socket.write(total_file_contents)
-      @socket.close_write
-
-    rescue Exception => e
-      @socket.close
-      raise e
+    read_file(file_path, PACKET_SIZE) do |data, index|
+      @socket.send(data, 0)
     end
+    @socket.flush
+    @socket.close_write
   end
 
   def receive
     conn = @socket
     total_content = ''
-    begin
-      while (content = conn.recv(1024)) != ''
-        total_content += content
-      end
-      conn.close
-      return total_content
-    rescue Exception => e
-      conn.close
-      raise e
+    thread = Thread.current
+    thread[:ready] = false
+    Thread.new do
+      sleep 0.01
+      thread[:ready] = true
     end
+    conn.wait_readable
+    while (content = conn.recv(PACKET_SIZE)) != ''
+      total_content += content
+    end
+    conn.close
+    total_content
   end
 end
